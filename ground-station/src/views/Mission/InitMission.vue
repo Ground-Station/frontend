4b8a4c9669752d341ac678be6ac023c9e46ef198
João Henrique Egewarth <egewarth@gmail.com>
2020-11-16 10:03:23 -0300

    criando pagina de confirmacao de missao

---
+++ ground-station/src/views/Mission/InitMission.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="md-layout md-alignment-center-center">
+      <md-card class="md-layout-item md-size-50 md-small-size-100 ">
+        <md-card-header>
+          <div class="md-title">Vamos iniciar uma missão?</div>
+        </md-card-header>
+      
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="cancel" class="md-accent" :disabled="sending">Agora não</md-button>
+          <router-link to="/chooseHardware" tag="md-button">Sim, vamos!</router-link>
+        </md-card-actions>
+      </md-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.md-card{
+  top: 80px;
+}
+body {
+  max-height: 100vh;
+}
+</style>
