237346937c80ca6f1e98bbdcf13a61c1b04b352c
João Henrique Egewarth <egewarth@gmail.com>
2020-11-22 11:31:11 -0300

    criando componente de dados de gps e pagina de pouso

---
+++ ground-station/src/views/Mission/Landing.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="md-layout md-alignment-top-space-around nowrap" id="container">
+      <bateryData>
+      </bateryData>
+      <heightData>
+      </heightData>
+    </div>
+    <div class="md-layout md-alignment-top-space-around nowrap" id="container">
+      <gpsData>
+      </gpsData>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  #container > .md-layout {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  min-height: 380px;
+  height: 380px;
+  margin-bottom: 30px;
+}
+</style>
+
+<script>
+import BateryData from './MissionData/Batery.vue'
+import HeightData from './MissionData/Height.vue'
+import GpsData from './MissionData/Gps.vue'
+export default {
+  name: 'LandingData',
+  components: {BateryData, HeightData, GpsData},
+}
+</script>
---
+++ ground-station/src/views/Mission/MissionData/Gps.vue
@@ -0,0 +1,69 @@
+<template>
+  <md-card class="md-layout-item md-layout md-size-90">
+    <md-card-header>
+      <div class="md-title">Foguete em pouso</div>
+    </md-card-header>
+    <md-card-content>
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-60">
+          <div id="chart">
+            <apexchart type="line" height="120" :options="chartOptions" :series="series"></apexchart>
+          </div>
+        </div>
+        <div class="md-layout-item md-size-30">
+          <span class="md-display-1">{{ coordinates }}</span>
+          <span class="md-headline">Coordenadas</span>
+          <md-button class="md-raised md-primary" @click="startFueling()">Finalizar Missão</md-button>
+        </div>
+      </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+export default {
+  name: 'HeightData',
+  components: { apexchart: VueApexCharts},
+  data: () => ({  
+    fueling: true,
+    coordinates: '-15.989383919844288, -48.045136048213',
+    series: [{
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    chartOptions: {
+      chart: {
+        height: 240,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      }
+    },
+  }),
+}
+</script>
