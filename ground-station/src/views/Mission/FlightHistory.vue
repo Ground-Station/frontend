3a5f8f748ff55d4b3c1c066650fc9c8c9f3da603
João Henrique Egewarth <egewarth@gmail.com>
2020-11-22 11:31:47 -0300

    criando componente de historico de voo e pagina de detalhe de missao

---
+++ ground-station/src/views/Mission/FlightHistory.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="md-layout md-alignment-center-space-around">
+      <apogee>
+      </apogee>
+      <heightHistory>
+      </heightHistory>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
+
+<script>
+import HeightHistory from './MissionData/HeightHistory.vue'
+import Apogee from './MissionData/Apogee.vue'
+export default {
+  name: 'FlightHistory',
+  components: {HeightHistory, Apogee},
+}
+</script>
---
+++ ground-station/src/views/Mission/MissionDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <sidebar pageTitle="Missão x">
+  <div>
+      <flightHistory>
+        </flightHistory>
+
+  </div>
+  </sidebar>
+</template>
+
+<script>
+ import Sidebar from '../../components/Sidebar.vue'
+//  import FuelingData from './Fueling.vue'
+ import FlightHistory from './FlightHistory.vue'
+//  import LandingData from './Landing.vue'
+  export default {
+    name: 'MissionDetail',
+    components: { Sidebar, FlightHistory},
+    data: () => ({
+      active: 'fueling',
+      fueling: false,
+      ignition: false,
+      flight: false,
+      landing: false,
+      ignitionStepError: null
+    }),
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
