64543659de11be1a48a89696e18be9ec2bd9c521
João Henrique Egewarth <egewarth@gmail.com>
2020-11-22 11:30:00 -0300

    criando componente de historico de peso e altitude

---
+++ ground-station/src/views/Mission/MissionData/HeightHistory.vue
@@ -0,0 +1,68 @@
+<template>
+  <md-card class="md-layout-item md-layout md-size-60 md-small-size-100">
+    <md-card-header>
+      <div class="md-title">Abastecimento do foguete</div>
+    </md-card-header>
+    <md-card-content>
+      <div class="md-layout md-gutter md-alignment-center-space-around">
+        <div class="md-layout-item md-size-70 md-small-size-100">
+          <div id="chart">
+            <apexchart type="line" height="240" :options="chartOptions" :series="series"></apexchart>
+          </div>
+        </div>
+        <div class="md-layout-item md-size-20 md-small-size-100">
+          <span class="md-display-1">{{ height }} metros</span>
+          <span class="md-headline">Altitude</span>
+        </div>
+      </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+export default {
+  name: 'HeightHistory',
+  components: { apexchart: VueApexCharts},
+  data: () => ({  
+    fueling: true,
+    height: 16,
+    series: [{
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    chartOptions: {
+      chart: {
+        height: 240,
+        type: 'line',
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      title: {
+        text: 'Product Trends by Month',
+        align: 'left'
+      },
+      grid: {
+        row: {
+          colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+          opacity: 0.5
+        },
+      },
+      xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      }
+    },
+  }),
+}
+</script>
---
+++ ground-station/src/views/Mission/MissionData/WeightHistory.vue
@@ -0,0 +1,108 @@
+<template>
+  <md-card class="md-layout-item md-size-60 md-small-size-100">
+    <md-card-header>
+      <div class="md-title">Abastecimento do foguete</div>
+    </md-card-header>
+    <md-card-content>
+      <div class="md-layout md-gutter md-alignment-center-space-around">
+        <div class="md-layout-item md-size-60 md-small-size-100">
+          <div id="chart">
+            <apexchart type="radialBar" height="240" :options="weightOptions" :series="weightSeries"></apexchart>
+          </div>
+        </div>
+        <div class="md-layout-item md-size-30 md-small-size-100">
+          <span class="md-display-1">{{ weight }} Kg</span>
+          <span class="md-headline">Peso</span>
+        </div>
+      </div>
+    </md-card-content>
+  </md-card>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+export default {
+  name: 'WeightHistory',
+  components: { apexchart: VueApexCharts},
+  data: () => ({  
+    fueling: true,
+    weight: 16,
+    weightSeries: [75],
+    weightOptions: {
+      chart: {
+        height: 240,
+        type: 'radialBar',
+        toolbar: {
+          show: true
+        }
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 225,
+            hollow: {
+            margin: 0,
+            size: '70%',
+            background: '#fff',
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: 'front',
+            dropShadow: {
+              enabled: true,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24
+            }
+          },
+          track: {
+            background: '#fff',
+            strokeWidth: '67%',
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35
+            }
+          },
+          dataLabels: {
+            show: true,
+            name: {
+              show: false
+            },
+            value: {
+              color: '#111',
+              fontSize: '36px',
+              show: true,
+            }
+          }
+        }
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'dark',
+          type: 'horizontal',
+          shadeIntensity: 0.5,
+          gradientToColors: ['#ABE5A1'],
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100]
+        }
+      },
+      stroke: {
+        lineCap: 'round'
+      },
+      labels: ['Bateria'],
+    },
+  }),
+}
+</script>
